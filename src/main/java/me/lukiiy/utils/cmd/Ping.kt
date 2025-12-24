72ab9e16343134d2f19a514a797b81691982f36d
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2025-12-24 00:42:33 -0300

    Update Ping.kt

--- src/main/java/me/lukiiy/utils/cmd/Ping.kt
+++ src/main/java/me/lukiiy/utils/cmd/Ping.kt
@@ -5,28 +5,34 @@ import com.mojang.brigadier.tree.LiteralCommandNode
 import io.papermc.paper.command.brigadier.CommandSourceStack
 import io.papermc.paper.command.brigadier.Commands
 import io.papermc.paper.command.brigadier.argument.ArgumentTypes
-import io.papermc.paper.command.brigadier.argument.resolvers.selector.PlayerSelectorArgumentResolver
 import me.lukiiy.utils.Defaults
-import net.kyori.adventure.text.Component
+import me.lukiiy.utils.help.Utils.asFancyString
+import me.lukiiy.utils.help.Utils.getPlayerOrThrow
 import net.kyori.adventure.text.format.TextDecoration
+import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 
 object Ping {
     private val main = Commands.literal("ping")
         .then(Commands.argument("player", ArgumentTypes.player())
             .executes {
-                val sender = it.source.sender
-                val target = it.getArgument("player", PlayerSelectorArgumentResolver::class.java).resolve(it.source).stream().findFirst().orElse(null) ?: throw Defaults.NOT_FOUND
+                val target = it.getPlayerOrThrow("player")
 
-                sender.sendMessage(Defaults.neutral(target.name().append(Component.text("'s ping is ").append(Component.text(target.ping).color(Defaults.PURPLE).decorate(TextDecoration.UNDERLINED)).append(Component.text(" ms")))))
+                handle(it.source.sender, target)
                 Command.SINGLE_SUCCESS
             })
         .executes {
-            val sender = it.source.sender as? Player ?: throw Defaults.NON_PLAYER
+            val target = it.source.sender as? Player ?: throw Defaults.NON_PLAYER
 
-            sender.sendMessage(Defaults.neutral(Component.text("Your ping is ").append(Component.text(sender.ping).color(Defaults.PURPLE).decorate(TextDecoration.UNDERLINED)).append(Component.text(" ms"))))
+            handle(target, target)
             Command.SINGLE_SUCCESS
         }
 
     fun register(): LiteralCommandNode<CommandSourceStack> = main.build()
+
+    fun handle(sender: CommandSender, player: Player) {
+        val pre = if (sender == player) "Your".asFancyString().color(Defaults.YELLOW) else player.name().color(Defaults.YELLOW).append("'s".asFancyString())
+
+        sender.sendMessage(Defaults.neutral(pre.append(" ping is ".asFancyString()).append("${player.ping}".asFancyString().color(Defaults.PURPLE).decorate(TextDecoration.UNDERLINED)).append(" ms".asFancyString())))
+    }
 }
